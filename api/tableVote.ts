c3d55c0b4c71b15d56663ded083cc6f288ec9655
sawaddepomtiw <tiwloso11233@gmail.com>
2024-03-17 01:23:40 +0700

    comment

--- api/tableVote.ts
+++ api/tableVote.ts
@@ -40,6 +40,28 @@ router.post("/insertVote", (req, res) => {
         res.status(201).send("error!");
     }
 });
+router.post("/insertVote/noID", (req, res) => {
+
+    if (req.query){
+
+        const vote: VoteModel = req.body;
+        let sql = "INSERT INTO vote( imid, status, score) VALUES (?,?,?)";
+        sql = mysql.format(sql, [
+            vote.imid,
+            vote.status,
+            vote.score
+        ]);
+        dbconn.query(sql, (err, result) =>{
+            if (err) throw err;
+            res.status(201).json({
+                affected_rows: result.affectedRows,
+                last_idx: result.insertId
+            });
+        });
+    } else {
+        res.status(201).send("error!");
+    }
+});
 
 router.get("/staticVote", (req, res) =>{
     if (req.query){
